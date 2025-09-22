63e86e5ecce122798655dc092d14a18cdb95a9b3
Marta Savchuk <marta.savchuk5@gmail.com>
2025-09-22 14:28:21 +0300

    feat: adds todo to factory file

--- src/generator/factory.ts
+++ src/generator/factory.ts
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker'
 import { getRandomItemFromArray } from '../utils/data-helpers'
 import { Entities } from './entities'
 import { Types } from './types'
-
+// TODO: add parameters to modify client data
 export function generateClient(): Types.CLIENT {
   const name = faker.company.name()
   const priority = getRandomItemFromArray(Entities.priorityList)
