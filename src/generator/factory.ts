3d838bd1f9660e32409c23157863689d4e39b5cf
Marta Savchuk <marta.savchuk5@gmail.com>
2025-10-05 11:21:09 +0200

    adds parameters to modify client data

--- src/generator/factory.ts
+++ src/generator/factory.ts
@@ -2,8 +2,12 @@ import { faker } from '@faker-js/faker'
 import { getRandomItemFromArray } from '../utils/data-helpers'
 import { Entities } from './entities'
 import { Types } from './types'
-// TODO: add parameters to modify client data
-export function generateClient(options?: {type?: string}): Types.CLIENT {
+export function generateClient(options?: {
+  name?: string
+  priority?: string
+  type?: string
+  isChargeCodeRequired?: boolean
+}): Types.CLIENT {
   const name = faker.company.name()
   const priority = getRandomItemFromArray(Entities.priorityList)
   const type = getRandomItemFromArray(Entities.typeList)
